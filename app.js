b8e7d66d08da35466df95549627e04a1e5374b40
amoswijaya <amoswijaya728@gmail.com>
2021-02-19 14:09:31 +0700

    siap deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = require('./routes/index')
 const app = express()
 const http = require('http').Server(app)
-const PORT = 3000
+const PORT = process.env.PORT||3000
 const cors = require('cors')
 
 app.use(cors())
